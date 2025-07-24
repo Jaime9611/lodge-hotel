06afb18919d87015e00b99b1bdfcec50739df0df
Jaime9611 <jaimeescobar9611@gmail.com>
2025-07-24 07:00:58 -0500

    Added simple security config

--- lodge-hotel-backend/security-service/src/main/java/com/lodge/security_service/config/SecurityConfig.java
+++ lodge-hotel-backend/security-service/src/main/java/com/lodge/security_service/config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package com.lodge.security_service.config;
 
+import com.lodge.security_service.service.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailService customUserDetailService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(AbstractHttpConfigurer::disable);
+        
+        return http.build();
+    }
 }
