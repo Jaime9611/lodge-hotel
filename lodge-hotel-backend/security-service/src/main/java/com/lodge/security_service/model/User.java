909cf0b5a68e11f82a97e84837d79b18db537534
Jaime9611 <jaimeescobar9611@gmail.com>
2025-07-24 06:40:58 -0500

    Added user model

---
+++ lodge-hotel-backend/security-service/src/main/java/com/lodge/security_service/model/User.java
@@ -0,0 +1,25 @@
+package com.lodge.security_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
