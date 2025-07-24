ffad4683a30fcbcc5efb842f091b60e2490b02fe
Jaime9611 <jaimeescobar9611@gmail.com>
2025-07-24 17:15:26 -0500

    Added controller for register users

--- lodge-hotel-backend/security-service/src/main/java/com/lodge/security_service/controller/UserController.java
+++ lodge-hotel-backend/security-service/src/main/java/com/lodge/security_service/controller/UserController.java
@@ -1,7 +1,23 @@
 package com.lodge.security_service.controller;
 
+import com.lodge.security_service.model.UserEntity;
+import com.lodge.security_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserEntity user) {
+        return ResponseEntity.ok(userService.registerUser(user));
+    }
 }
