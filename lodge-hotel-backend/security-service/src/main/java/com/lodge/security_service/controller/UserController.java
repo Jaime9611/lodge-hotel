d0650ac56eb6e8de56735e211b36f487e5c07fef
Jaime9611 <jaimeescobar9611@gmail.com>
2025-07-24 16:45:17 -0500

    Added user service

---
+++ lodge-hotel-backend/security-service/src/main/java/com/lodge/security_service/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.lodge.security_service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ lodge-hotel-backend/security-service/src/main/java/com/lodge/security_service/service/UserService.java
@@ -0,0 +1,75 @@
+package com.lodge.security_service.service;
+
+import com.lodge.security_service.model.UserEntity;
+import com.lodge.security_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtEncoder jwtEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    private static final int EXPIRATION_TIME_SEC = 3600;
+
+    public String registerUser(UserEntity user) {
+        Optional<UserEntity> userEntity = userRepository.findByUsername(user.getUsername());
+        if (userEntity.isPresent()) {
+            return "Username already taken";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+
+        return "User Registered successfully.";
+    }
+
+    public Map<String, Object> login(String username, String password) {
+        Optional<UserEntity> userEntity = userRepository.findByUsername(username);
+        Map<String, Object> response = new HashMap<>();
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        if (userEntity.isEmpty()) {
+            response.put("status", "User not found.");
+            return response;
+        }
+
+        String accessToken = generateToken(userEntity.get(), authentication);
+        response.put("access_token", accessToken);
+        response.put("expires_in", EXPIRATION_TIME_SEC);
+
+        return response;
+    }
+
+    private String generateToken(UserEntity userEntity, Authentication authentication) {
+        Instant now = Instant.now();
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .issuer("lodge-api")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(EXPIRATION_TIME_SEC))
+                .subject(authentication.getName())
+                .claim("role", authentication.getAuthorities().toString())
+                .claim("user", userEntity.getUsername())
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+    }
+}
