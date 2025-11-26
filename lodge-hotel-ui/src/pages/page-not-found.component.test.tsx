cd4272418d356daf3ee0c93314b3c867b57f65b8
Jaime9611 <jaimeescobar9611@gmail.com>
2025-11-26 17:49:27 -0500

    Added settings page test

--- lodge-hotel-ui/src/pages/page-not-found.component.test.tsx
+++ lodge-hotel-ui/src/pages/page-not-found.component.test.tsx
@@ -1,7 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 import PageNotFound from "./page-not-found.component";
-import type { useMoveBack } from "@hooks";
 
 const mockHook = vi.hoisted(() => {
   return {
---
+++ lodge-hotel-ui/src/pages/settings.component.test.tsx
@@ -0,0 +1,29 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Settings from "./settings.component";
+import { MemoryRouter } from "react-router-dom";
+
+vi.mock("../features/settings/use-settings.hook", () => ({
+  useSettings: () => ({ isLoading: false, settings: {} }),
+}));
+vi.mock("../features/settings/use-update-settings.hook", () => ({
+  useUpdateSetting: () => ({ isUpdating: false, updateSettings: vi.fn() }),
+}));
+describe("Settings Component", {}, () => {
+  it("renders the Settings page", () => {
+    render(
+      <MemoryRouter>
+        <Settings />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Settings")).toBeInTheDocument();
+    expect(screen.getByLabelText(/Minimum nights/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Maximum nights/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Select new logo/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /cancel/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /update settings/i })
+    ).toBeInTheDocument();
+  });
+});
